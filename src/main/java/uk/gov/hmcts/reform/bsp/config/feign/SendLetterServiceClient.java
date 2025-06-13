5d7a53cf89531e1892cb17d6dd90efbfc8ddc816
amstevenson <addstevenson@hotmail.com>
2025-06-13 15:52:02 +0100

    more models

--- src/main/java/uk/gov/hmcts/reform/bsp/config/feign/SendLetterServiceClient.java
+++ src/main/java/uk/gov/hmcts/reform/bsp/config/feign/SendLetterServiceClient.java
@@ -1,9 +1,11 @@
 package uk.gov.hmcts.reform.bsp.config.feign;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
+import uk.gov.hmcts.reform.bsp.models.StaleLetterResponse;
 
 @FeignClient(
     name = "sendLetterServiceClient",
@@ -11,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestHeader;
 )
 public interface SendLetterServiceClient {
 
-    //        @GetMapping("/stale-letters")
-    //        List<LetterEntry> getStaleLetters();
+    @GetMapping("/stale-letters")
+    StaleLetterResponse getStaleLetters();
 
     @PutMapping("/letters/{letterId}/mark-aborted")
     void markAborted(@PathVariable("letterId") String letterId,
