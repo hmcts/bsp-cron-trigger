2e1eb57cdbe38f11da18dfeb001ab2855e10287a
hurricanepilot <jonathan.coomber@justice.gov.uk>
2026-02-18 08:21:14 +0000

    Add tests for altered code

--- src/test/java/uk/gov/hmcts/reform/bsp/services/BulkPrintChecksServiceTest.java
+++ src/test/java/uk/gov/hmcts/reform/bsp/services/BulkPrintChecksServiceTest.java
@@ -1,23 +1,6 @@
 package uk.gov.hmcts.reform.bsp.services;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.ArgumentCaptor;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import uk.gov.hmcts.reform.bsp.config.AuthorisationProperties;
-import uk.gov.hmcts.reform.bsp.config.feign.SendLetterServiceClient;
-import uk.gov.hmcts.reform.bsp.integrations.SlackMessageHelper;
-import uk.gov.hmcts.reform.bsp.models.CheckPostedTaskResponse;
-import uk.gov.hmcts.reform.bsp.models.StaleLetter;
-import uk.gov.hmcts.reform.bsp.models.StaleLetterResponse;
-
-import java.time.LocalDateTime;
-import java.util.Collections;
-import java.util.List;
-import java.util.UUID;
-
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -28,6 +11,27 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import uk.gov.hmcts.reform.bsp.config.AuthorisationProperties;
+import uk.gov.hmcts.reform.bsp.config.feign.SendLetterServiceClient;
+import uk.gov.hmcts.reform.bsp.integrations.SlackMessageHelper;
+import uk.gov.hmcts.reform.bsp.models.CheckPostedTaskResponse;
+import uk.gov.hmcts.reform.bsp.models.PostedReportTaskResponse;
+import uk.gov.hmcts.reform.bsp.models.StaleLetter;
+import uk.gov.hmcts.reform.bsp.models.StaleLetterResponse;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
 class BulkPrintChecksServiceTest {
@@ -123,6 +127,101 @@ class BulkPrintChecksServiceTest {
     void runDailyChecks_withNullSlackClient_shouldThrowNullPointerException() {
         BulkScanChecksService serviceWithNull = new BulkScanChecksService(null, null, null, null, null);
         assertThrows(NullPointerException.class, serviceWithNull::runDailyChecks);
+    }
+
+    @Test
+    void runDailyChecks_shouldSendMessageWithProcessReportsDetails() {
+        LocalDate now = LocalDate.now();
+        PostedReportTaskResponse prtrA = new PostedReportTaskResponse("SSCS-IB", now, false);
+        prtrA.setMarkedPostedCount(100);
+        PostedReportTaskResponse prtrB = new PostedReportTaskResponse("SSCS-IB", now, false);
+        prtrB.setProcessingFailed(true);
+        prtrB.setErrorMessage("error message");
+        PostedReportTaskResponse prtrC = new PostedReportTaskResponse("SSCS-REFORM", now, false);
+        prtrA.setMarkedPostedCount(100);
+        PostedReportTaskResponse prtrD = new PostedReportTaskResponse("NFDIV", now, true);
+        prtrA.setMarkedPostedCount(100);
+
+        when(letterClient.runProcessReports(anyString())).thenReturn(
+          List.of(prtrA, prtrB, prtrC, prtrD)
+        );
+
+        when(letterClient.runCheckPosted(any(String.class))).thenReturn(new CheckPostedTaskResponse(0));
+        when(letterClient.getStaleLetters()).thenReturn(new StaleLetterResponse(0, Collections.emptyList()));
+
+        bulkPrintChecksService.runDailyChecks();
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(slackHelper, times(1)).sendLongMessage(captor.capture());
+
+        String actual = captor.getValue();
+        assertThat(actual).contains(
+            "Bulk Print",
+            "SSCS-IB (domestic) complete; 100 letters marked as posted",
+            "SSCS-IB (domestic) ERROR: error message",
+            "SSCS-REFORM (domestic) complete; 0 letters marked as posted",
+            "NFDIV (international) complete; 0 letters marked as posted"
+            );
+    }
+
+    @Test
+    void runDailyChecks_shouldSendMessageWithCheckPostedDetails() {
+        when(letterClient.runProcessReports(anyString())).thenReturn(Collections.emptyList());
+        when(letterClient.runCheckPosted(any(String.class))).thenReturn(new CheckPostedTaskResponse(30));
+        when(letterClient.getStaleLetters()).thenReturn(new StaleLetterResponse(0, Collections.emptyList()));
+
+        bulkPrintChecksService.runDailyChecks();
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(slackHelper, times(1)).sendLongMessage(captor.capture());
+
+        String actual = captor.getValue();
+        assertThat(actual).contains(
+            "Bulk Print",
+            "30 letters were marked as NO_REPORT_ABORTED."
+        );
+    }
+
+    @Test
+    void runDailyChecks_abortsAndDoesntSendMessageWhenProcessReportsCallFails() {
+        when(letterClient.runProcessReports(anyString())).thenThrow(new RuntimeException());
+
+        assertThrows(IllegalStateException.class, () -> bulkPrintChecksService.runDailyChecks());
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(slackHelper, times(1)).sendLongMessage(captor.capture());
+
+        String actual = captor.getValue();
+        assertThat(actual).contains("Could not run process reports task");
+    }
+
+    @Test
+    void runDailyChecks_abortsAndDoesntSendMessageWhenCheckPostedCallFails() {
+        when(letterClient.runProcessReports(anyString())).thenReturn(Collections.emptyList());
+        when(letterClient.runCheckPosted(any(String.class))).thenThrow(new RuntimeException());
+
+        assertThrows(IllegalStateException.class, () -> bulkPrintChecksService.runDailyChecks());
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(slackHelper, times(1)).sendLongMessage(captor.capture());
+
+        String actual = captor.getValue();
+        assertThat(actual).contains("Could not run check posted task");
+    }
+
+    @Test
+    void runDailyChecks_abortsAndDoesntSendMessageWhenGetStaleLettersCallFails() {
+        when(letterClient.runProcessReports(anyString())).thenReturn(Collections.emptyList());
+        when(letterClient.runCheckPosted(any(String.class))).thenReturn(new CheckPostedTaskResponse(30));
+        when(letterClient.getStaleLetters()).thenThrow(new RuntimeException());
+
+        assertThrows(IllegalStateException.class, () -> bulkPrintChecksService.runDailyChecks());
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(slackHelper, times(1)).sendLongMessage(captor.capture());
+
+        String actual = captor.getValue();
+        assertThat(actual).contains("Could not fetch stale letters");
     }
 }
 
